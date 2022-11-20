23b639971db33d6b7f25f05123b96be967009cc0
Your Name <you@example.com>
2022-11-20 15:30:52 +0200

    refactor

--- src/components/TenDaysInfo/TenDaysInfo.tsx
+++ src/components/TenDaysInfo/TenDaysInfo.tsx
@@ -1,22 +1,21 @@
 import {ReactElement} from 'react';
 import {useAppSelector} from "../../hooks/hooks";
 import {getWeekDay} from "../../libs/getWeekDay";
+import Svg from 'react-inlinesvg';
 import styles from './TenDaysInfo.module.scss'
-
 export const TenDaysInfo = ():ReactElement =>{
     const {tenDaysArray} = useAppSelector(state => state.Weather)
-    // console.log(getWeekDay(new Date('2022-11-21')))
+    // {getWeekDay(new Date(item.date))}
+    // <p>{item.day.maxtemp_c}</p>
+    // <p>{item.day.mintemp_c}</p>
+    // {tenDaysArray && tenDaysArray.length > 0 && tenDaysArray.map((item) : any=>{
+    //     return (
+    //
+    //     )
+    // })}
     return (
         <div className={styles.wrapper}>
-            {tenDaysArray && tenDaysArray.length > 0 && tenDaysArray.map((item) : any=>{
-                return (
-                    <div>
-                        {getWeekDay(new Date(item.date))}
-                        <p>{item.day.maxtemp_c}</p>
-                        <p>{item.day.mintemp_c}</p>
-                    </div>
-                )
-            })}
+            <Svg src='/icons/kalendar.svg'/>
         </div>
     )
 
