2fcc28eb50bc4fadc5a94ecb67ff3826a2e837e4
Your Name <you@example.com>
2022-11-21 22:31:02 +0200

    test

---
+++ src/components/MoreInfo/MoreInfo.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react'
+import { useAppSelector } from '../../hooks/hooks'
+import { s } from './MoreInfo.module.scss'
+
+export const MoreInfo = (): ReactElement => {
+	const data = useAppSelector(state => state.Weather)
+	console.log(data)
+	return <div>
+		{/* 1 <UltravioletIndex /> */}
+		{/* 2 <SunriseAndSunset /> */}
+		{/* 3 <WindSpeed /> */}
+		{/* 4 <Precipitation /> */}
+		{/* 5 <Temp /> */}
+		{/* 6 <Humidity /> */}
+	</div>
+}
--- src/components/TenDaysInfo/TenDaysInfo.tsx
+++ src/components/TenDaysInfo/TenDaysInfo.tsx
@@ -1,11 +1,8 @@
-import {ReactElement} from 'react';
-import {useAppSelector} from "../../hooks/hooks";
+import { ReactElement } from 'react'
+import { useAppSelector } from '../../hooks/hooks'
 
-export const TenDaysInfo = ():ReactElement =>{
-    const data = useAppSelector(state => state.Weather)
-    console.log(data)
-    return (
-        <div>піська</div>
-    )
-
+export const TenDaysInfo = (): ReactElement => {
+	const data = useAppSelector(state => state.Weather)
+	console.log(data)
+	return <div></div>
 }
